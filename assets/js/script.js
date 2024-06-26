0483ea1a38d760a158763f1bc82eb7d851337df5
tigorsprado <tiago_sprado@hotmail.com>
2024-06-26 17:20:00 -0300

    init

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const botaoEstouComSorte = document.querySelector('form [type="button"]');
+
+botaoEstouComSorte.addEventListener("click", () =>  {
+
+    location.href = "https://class.hcode.com.br"
+
+})
